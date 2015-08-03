98b16b4ded18ff83608495a05ff4461338cc49cf
Slava Bacherikov <slava@bacher09.org>
2015-08-03 19:37:49 +0300

    Add getXtermColor function

--- src/System/Console/Xterm/Colors.hs
+++ src/System/Console/Xterm/Colors.hs
@@ -2,9 +2,13 @@
 module System.Console.Xterm.Colors
     ( RGB(..)
     , xtermColors
+    , getXtermColor
     ) where
 
 
+import Data.Word
+
+import Data.Vector.Unboxed ((!))
 import qualified Data.Vector.Unboxed as VU
 
 import System.Console.Xterm.Types
@@ -14,3 +18,7 @@ import qualified System.Console.Xterm.Colors.TH as TH
 -- | Unboxed Vectore that holds info about xterm colors
 xtermColors :: VU.Vector RGB
 xtermColors = $(let val = VU.fromList TH.xtermColors in [| val |])
+
+
+getXtermColor :: Word8 -> RGB
+getXtermColor n = xtermColors ! (fromIntegral n)
