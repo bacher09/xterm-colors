a5f2691f5b9a23442b9d095f2842b69ea82a593e
Slava Bacherikov <slava@bacher09.org>
2015-08-03 18:00:31 +0300

    Generate unboxed instance for rgb type

--- src/System/Console/Xterm/Types.hs
+++ src/System/Console/Xterm/Types.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TemplateHaskell, MultiParamTypeClasses, TypeFamilies #-}
 module System.Console.Xterm.Types
     ( RGB(..)
     , mkRGB
@@ -5,10 +6,17 @@ module System.Console.Xterm.Types
 
 
 import Data.Word
+import Data.Vector.Unboxed.Deriving
 
 
 newtype RGB = RGB (Word8, Word8, Word8)
     deriving(Show, Eq, Ord)
+
+
+derivingUnbox "RGB"
+    [t| RGB -> (Word8, Word8, Word8) |]
+    [| \(RGB (r, g, b)) -> (r, g, b) |]
+    [| \(r, g, b) -> RGB (r, g, b) |]
 
 
 mkRGB :: Word8 -> Word8 -> Word8 -> RGB
